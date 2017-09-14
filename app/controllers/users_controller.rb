4750215011be37e092cfdc7f420c2d371c83023a
risatino <charissatolentino@gmail.com>
2017-09-13 20:24:29 -0500

    practice rails 5 app

---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,21 @@
+class UsersController < ApplicationController
+  def new
+    
+  end
+
+  def create
+    user = User.new(
+                    name: params[:name],
+                    email: params[:email],
+                    password: params[:password],
+                    password_confirmation: params[:password_confirmation]
+                    ) 
+    if user.save
+      session[:user_id] = user.id
+      #successfully created account
+    else
+      
+    end 
+  end
+
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,19 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  get '/' => 'clowns#index'
+  get '/clowns' => 'clowns#index'
+
+  get '/clowns/random' => 'clowns#random'
+  get '/clowns/chat' => 'clowns#chat'
+  get '/story' => 'clowns#story'
+
+  get '/clowns/new' => 'clowns#new'
+  post '/clowns' => 'clowns#create'
+  get '/clowns/:id' => 'clowns#show'
+  get '/clowns/:id/edit' => 'clowns#edit'
+  patch '/clowns/:id' => 'clowns#update'
+
+  delete '/clowns/:id' => 'clowns#destroy'
+
+  get '/signup' => 'users#new'
+  post '/users' => 'users#create'
 end
